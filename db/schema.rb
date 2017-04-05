22827596f8b7ecb10828f5fb04267a2e6a84174a
mazikwyry <a.mazur@exlabs.co.uk>
2017-04-05 14:53:49 +0200

    add request model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170405124257) do
+ActiveRecord::Schema.define(version: 20170405125104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,16 @@ ActiveRecord::Schema.define(version: 20170405124257) do
     t.index ["user_id"], name: "index_projects_on_user_id", using: :btree
   end
 
+  create_table "requests", force: :cascade do |t|
+    t.integer  "endpoint_id"
+    t.integer  "status"
+    t.jsonb    "body"
+    t.jsonb    "body_draft"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["endpoint_id"], name: "index_requests_on_endpoint_id", using: :btree
+  end
+
   create_table "url_params", force: :cascade do |t|
     t.integer  "endpoint_id"
     t.string   "key"
@@ -77,5 +87,6 @@ ActiveRecord::Schema.define(version: 20170405124257) do
   add_foreign_key "groups", "groups"
   add_foreign_key "groups", "projects"
   add_foreign_key "projects", "users"
+  add_foreign_key "requests", "endpoints"
   add_foreign_key "url_params", "endpoints"
 end

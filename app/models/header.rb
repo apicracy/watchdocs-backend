7f1ddc5eedd3cba73115abf545e6a34fb27c619e
mazikwyry <a.mazur@exlabs.co.uk>
2017-04-14 14:44:56 +0200

    add headers statuses

--- app/models/header.rb
+++ app/models/header.rb
@@ -1,6 +1,8 @@
 class Header < ApplicationRecord
   belongs_to :headerable, polymorphic: true
 
+  enum status: %i(fresh up_to_date outdated stale)
+
   def update_required(new_required)
     if required.present?
       update(required_draft: new_required)

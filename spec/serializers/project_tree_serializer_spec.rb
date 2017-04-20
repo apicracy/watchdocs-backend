0bd71f2b24d6806ec2419819f62e330636a51cc7
mazikwyry <a.mazur@exlabs.co.uk>
2017-04-20 10:11:31 +0200

    rename tree serializer

---
+++ spec/serializers/project_tree_serializer_spec.rb
@@ -0,0 +1,76 @@
+require 'rails_helper'
+
+# TODO: Refactor to user json schema
+RSpec.describe ProjectTreeSerializer, type: :serializer do
+  subject(:serializer) { described_class.new(project) }
+  let(:project) { Fabricate :project }
+
+  describe '#tree' do
+    subject(:json_tree) { JSON.parse(serializer.to_json)['tree'] }
+
+    it 'returns not grouped endpoints and docs' do
+      endpoint1 = Fabricate :endpoint, url: '/contributions', project: project
+      endpoint2 = Fabricate :endpoint, url: '/pledges', project: project
+      document = Fabricate :document, project: project
+
+      expected_json = [
+        {
+          'id' => endpoint1.id,
+          'type' => 'Endpoint',
+          'url' => endpoint1.url,
+          'method' => endpoint1.http_method
+        },
+        {
+          'id' => endpoint2.id,
+          'type' => 'Endpoint',
+          'url' => endpoint2.url,
+          'method' => endpoint2.http_method
+        },
+        {
+          'id' => document.id,
+          'type' => 'Document',
+          'name' => document.name,
+          'text' => document.text
+        }
+      ]
+
+      expect(json_tree).to eq(expected_json)
+    end
+
+    it 'returns grouped endpoints and docs' do
+      group = Fabricate :group, project: project
+      endpoint1 = Fabricate :endpoint, url: '/contributions', group: group
+      endpoint2 = Fabricate :endpoint, url: '/contributions/:id', group: group
+      document = Fabricate :document, project: project, group: group
+
+      expected_json = [{
+        'id' => group.id,
+        'type' => 'Group',
+        'name' => group.name,
+        'description' => group.description,
+        'items' => [
+          {
+            'id' => endpoint1.id,
+            'type' => 'Endpoint',
+            'url' => endpoint1.url,
+            'method' => endpoint1.http_method
+          },
+          {
+            'id' => endpoint2.id,
+            'type' => 'Endpoint',
+            'url' => endpoint2.url,
+            'method' => endpoint2.http_method
+          },
+          {
+            'id' => document.id,
+            'type' => 'Document',
+            'name' => document.name,
+            'text' => document.text
+          }
+        ]
+      }]
+
+      expect(json_tree).to eq(expected_json)
+    end
+  end
+end

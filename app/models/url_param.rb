7705a619e5d52142ae2aec0a73b8ca78b60cffe6
Krzysztof Szromek <k.szromek@exlabs.pl>
2017-04-20 16:38:01 +0200

    Add validations to URL param

--- app/models/url_param.rb
+++ app/models/url_param.rb
@@ -1,6 +1,10 @@
 class UrlParam < ApplicationRecord
   belongs_to :endpoint
 
+  validates :key,
+            presence: true,
+            uniqueness: { scope: :endpoint_id }
+
   enum status: %i(fresh up_to_date outdated stale)
 
   def update_required(new_required)

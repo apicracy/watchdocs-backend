30f34df8c8383f376ab048b784a5fb6c6c5358bb
Krzysztof Szromek <k.szromek@exlabs.pl>
2017-04-24 16:45:03 +0200

    Rename columns

--- spec/models/url_param_spec.rb
+++ spec/models/url_param_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe UrlParam, type: :model do
   subject(:url_param) { Fabricate.build :url_param }
 
   describe '#valid?' do
-    it { is_expected.to validate_uniqueness_of(:key).scoped_to(:endpoint_id) }
+    it { is_expected.to validate_uniqueness_of(:name).scoped_to(:endpoint_id) }
     it { is_expected.to validate_presence_of(:endpoint) }
   end
 end

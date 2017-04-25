42e7cad8cc1fa9a5bdd7cc4fc3c164051aaf481d
mazikwyry <a.mazur@exlabs.co.uk>
2017-04-25 12:19:32 +0200

    update edpoint request spec

---
+++ spec/requests/api/v1/endpoint/endpoint_update_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+RSpec.describe 'PUT /endpoints/:id', type: :request do
+  let(:endpoint) { Fabricate :endpoint }
+  let(:endpoint_id) { endpoint.id }
+  let(:url) { "/api/v1/endpoints/#{endpoint_id}" }
+  let(:params) do
+    {
+      url: '/api/v1/users/legal_identities',
+      http_method: 'POST',
+      title: Faker::Lorem.word,
+      summary: "<p>#{Faker::Lorem.paragraph}<p>"
+    }
+  end
+
+  context 'when user is unauthenticated' do
+    before { put url, params: params }
+    it_behaves_like 'unauthorized'
+  end
+
+  context 'when user is not the owner of the endpoint' do
+    before do
+      login_as Fabricate :user
+      put url, params: params
+    end
+
+    it_behaves_like 'not found'
+  end
+
+  context 'when endpoint does not exist' do
+    let(:endpoint_id) { -1 }
+
+    before do
+      login_as Fabricate :user
+      put url, params: params
+    end
+
+    it_behaves_like 'not found'
+  end
+
+  context 'when user is the owner of the endpoint' do
+    context 'and params are valid' do
+      before do
+        login_as endpoint.project.user, scope: :user
+        put url, params: params
+      end
+
+      it 'returns 200 and serialized updated endpoint' do
+        expect(response.status).to eq 200
+        expect(json).to eq(serialized(endpoint.reload))
+      end
+    end
+
+    context 'params are invalid' do
+      before do
+        params[:url] = nil
+        login_as project.user, scope: :user
+        put url, params: params
+      end
+
+      it_behaves_like 'invalid'
+    end
+  end
+end

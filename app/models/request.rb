41dfb975300d69d5e85b7c30d74b1cdfd7e0a2e8
mazikwyry <a.mazur@exlabs.co.uk>
2017-04-28 14:23:10 +0200

    remove concern

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,8 +1,13 @@
 class Request < ApplicationRecord
   include BodyAndHeadersUpdatable
-  include EndpointBelongable
 
   has_many :headers, as: :headerable, dependent: :destroy
+  belongs_to :endpoint
+
+  validates :endpoint,
+            presence: true
 
   enum status: %i(outdated up_to_date)
+
+  delegate :user, to: :endpoint
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -1,13 +1,18 @@
 class Response < ApplicationRecord
   include BodyAndHeadersUpdatable
-  include EndpointBelongable
+
+  belongs_to :endpoint
+  has_many :headers, as: :headerable, dependent: :destroy
+
+  validates :endpoint,
+            presence: true
 
   validates :http_status_code,
             presence: true,
             numericality: { only_integer: true },
             uniqueness: { scope: :endpoint_id }
 
-  has_many :headers, as: :headerable, dependent: :destroy
-
   enum status: %i(outdated up_to_date)
+
+  delegate :user, to: :endpoint
 end

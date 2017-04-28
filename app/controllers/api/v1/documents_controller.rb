c3df2a2ac6020e5713b0ef88e2b4d52cc3be4428
pavrin <d.jurowicz@exlabs.pl>
2017-04-28 15:21:36 +0200

    Delete document endpoint

--- app/controllers/api/v1/documents_controller.rb
+++ app/controllers/api/v1/documents_controller.rb
@@ -2,6 +2,9 @@ module Api
   module V1
     class DocumentsController < ApplicationController
       before_action :authenticate_user!
+      before_action :set_document, except: [:create]
+
+      load_and_authorize_resource except: [:create]
 
       def create
         document = Document.new(create_document_params)
@@ -10,7 +13,16 @@ module Api
         render_resource(document)
       end
 
+      def destroy
+        @document.destroy
+        render json: @document
+      end
+
       private
+
+      def set_document
+        @document = Document.find(params[:id])
+      end
 
       def create_document_params
         params.permit(:project_id, :name)

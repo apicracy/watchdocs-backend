e0f942ed68fdd4a326763800476a9a682c4314c6
mazikwyry <a.mazur@exlabs.co.uk>
2017-04-28 08:49:06 +0200

    implement headers validation

--- app/models/header.rb
+++ app/models/header.rb
@@ -1,6 +1,15 @@
 class Header < ApplicationRecord
   belongs_to :headerable, polymorphic: true
 
+  validates :key,
+            presence: true,
+            uniqueness: { scope: [:headerable_id, :headerable_type] }
+
+  validates :headerable,
+            :required,
+            :key,
+            presence: true
+
   enum status: %i(fresh up_to_date outdated stale)
 
   def update_required(new_required)

17153c82997057e9bbbe74d4ae6653c12f407c5e
pavrin <d.jurowicz@exlabs.pl>
2017-04-28 14:30:34 +0200

    Update document endpoint

--- app/controllers/api/v1/documents_controller.rb
+++ app/controllers/api/v1/documents_controller.rb
@@ -2,6 +2,9 @@ module Api
   module V1
     class DocumentsController < ApplicationController
       before_action :authenticate_user!
+      before_action :set_document, except: [:create]
+
+      load_and_authorize_resource except: [:create]
 
       def create
         document = Document.new(create_document_params)
@@ -10,10 +13,23 @@ module Api
         render_resource(document)
       end
 
+      def update
+        @document.update(update_document_params)
+        render_resource(@document)
+      end
+
       private
+
+      def set_document
+        @document = Document.find(params[:id])
+      end
 
       def create_document_params
         params.permit(:project_id, :name)
+      end
+
+      def update_document_params
+        params.permit(:name, :text)
       end
     end
   end
---
+++ spec/requests/api/v1/documents/document_update_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+RSpec.describe 'PUT /documents/:id', type: :request do
+  let(:document) { Fabricate :document }
+  let(:document_id) { document.id }
+  let(:url) { "/api/v1/documents/#{document_id}" }
+  let(:params) do
+    {
+      name: Faker::Lorem.word,
+      text: Faker::Lorem.paragraph
+    }
+  end
+
+  context 'when user is unauthenticated' do
+    before { put url, params: params }
+    it_behaves_like 'unauthorized'
+  end
+
+  context 'when user is not the owner of the document' do
+    before do
+      login_as Fabricate :user
+      put url, params: params
+    end
+
+    it_behaves_like 'forbidden'
+  end
+
+  context 'when document does not exist' do
+    let(:document_id) { -1 }
+
+    before do
+      login_as Fabricate :user
+      put url, params: params
+    end
+
+    it_behaves_like 'not found'
+  end
+
+  context 'when user is the owner of the document' do
+    let(:user) { document.project.user }
+
+    context 'and params are valid' do
+      before do
+        login_as user, scope: :user
+        put url, params: params
+      end
+
+      it 'returns 200 and serialized updated document' do
+        expect(response.status).to eq 200
+        expect(json).to eq(serialized(document.reload))
+      end
+    end
+
+    context 'params are invalid' do
+      before do
+        params[:name] = nil
+        login_as user, scope: :user
+        put url, params: params
+      end
+
+      it_behaves_like 'invalid'
+    end
+  end
+end

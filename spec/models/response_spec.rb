656a0f3e01846fff9930794a5266ea509ca5db58
mazikwyry <a.mazur@exlabs.co.uk>
2017-04-28 14:48:44 +0200

    syntax error fix

--- spec/models/response_spec.rb
+++ spec/models/response_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Response, type: :model do
     it { is_expected.to allow_value(type: 'array').for(:body) }
     it { is_expected.not_to allow_value(type: 'hash').for(:body) }
     it { is_expected.to allow_value(type: 'array').for(:body_draft) }
-    it { is_expected.not_to allow_value(type: 'hash').for(:body_draft) 
+    it { is_expected.not_to allow_value(type: 'hash').for(:body_draft) }
     it { is_expected.to validate_uniqueness_of(:http_status_code).scoped_to(:endpoint_id) }
     it { is_expected.to validate_presence_of(:http_status_code) }
     it { is_expected.to validate_numericality_of(:http_status_code).only_integer }

2a933993e531a0242060643828577a9486b7475c
pavrin <d.jurowicz@exlabs.pl>
2017-05-02 14:29:23 +0200

    Removed serializer and added group_id to params

--- app/serializers/group_serializer.rb
+++
@@ -1,9 +0,0 @@
-class GroupSerializer < ActiveModel::Serializer
-  attributes :id,
-             :project_id,
-             :group_id,
-             :name,
-             :description,
-             :created_at,
-             :updated_at
-end

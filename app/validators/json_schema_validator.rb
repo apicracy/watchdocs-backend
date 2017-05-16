0d41e502a3b9ac0009a8e9caeb598951acf547f8
Krzysztof <k.szromek@exlabs.pl>
2017-05-16 13:15:02 +0200

    Fix json schema validator

--- app/validators/json_schema_validator.rb
+++ app/validators/json_schema_validator.rb
@@ -7,7 +7,7 @@ class JsonSchemaValidator < ActiveModel::EachValidator
   )
 
   def validate_each(record, attribute, value)
-    return true if value.nil?
+    return true if value.blank?
     JSON::Validator.validate!(JSON_META_SCHEMA, value)
   rescue JSON::Schema::ValidationError => e
     record.errors[attribute] << "json-schema is invalid: #{e.message}"

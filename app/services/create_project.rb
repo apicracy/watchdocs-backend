cb1713daccd50b651877b73facf97bd9b4509ecb
mazikwyry <a.mazur@exlabs.co.uk>
2017-06-02 15:41:52 +0200

    refactoring inside create project service

--- app/services/create_project.rb
+++ app/services/create_project.rb
@@ -1,18 +1,29 @@
 class CreateProject
   attr_reader :project
 
-  def initialize(initail_params)
-    @project = Project.new(initail_params)
+  def initialize(initial_params)
+    @project = Project.new(initial_params)
   end
 
   def call
-    project.generate_api_credentials
-    begin
-      CreateProjectInReportsService.new(project).call
-      project.save
-    rescue ReportsServiceError => e
-      project.errors.add(:name, e.message)
-    end
+    generate_api_credentials
+    create_project_in_reports_service
     project
+  end
+
+  private
+
+  def generate_api_credentials
+    return if project.app_id && project.app_secret
+    credentials = ApiCredentials.generate(project.name)
+    return unless credentials
+    project.assign_attributes(credentials)
+  end
+
+  def create_project_in_reports_service
+    CreateProjectInReportsService.new(project).call
+    project.save
+  rescue ReportsServiceError => e
+    project.errors.add(:name, e.message)
   end
 end

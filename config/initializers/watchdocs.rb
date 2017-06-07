eb182c57c53a1d8e8e514ac733cabf6ce4cc310b
mazikwyry <a.mazur@exlabs.co.uk>
2017-06-07 12:26:44 +0200

    remove not needed files

--- config/initializers/watchdocs.rb
+++
@@ -1,4 +0,0 @@
-Watchdocs::Rails.configure do |c|
-  c.app_id = 'wdb'
-  c.app_secret = '9830123az'
-end
--- spec/support/watchdocs.rb
+++
@@ -1,9 +0,0 @@
-RSpec.configure do |config|
-  config.before(:suite) do
-    Watchdocs::Rails::Recordings.clear!
-  end
-
-  config.after(:suite) do
-    Watchdocs::Rails::Recordings.export
-  end
-end

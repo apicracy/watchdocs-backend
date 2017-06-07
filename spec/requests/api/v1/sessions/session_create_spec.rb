1047873f418f5f86471a0bc1ded3c479edcf2a2c
mazikwyry <a.mazur@exlabs.co.uk>
2017-06-07 15:03:39 +0200

    extend jwt lifetime to 24h and specs for that

--- spec/requests/api/v1/sessions/session_create_spec.rb
+++ spec/requests/api/v1/sessions/session_create_spec.rb
@@ -13,7 +13,9 @@ RSpec.describe 'POST /login', type: :request do
   end
 
   context 'when params are correct' do
-    before { post url, params: params }
+    before do
+      post url, params: params
+    end
 
     it 'returns 200' do
       expect(response).to have_http_status(200)
@@ -25,15 +27,26 @@ RSpec.describe 'POST /login', type: :request do
     end
 
     it 'returns valid JWT token' do
-      token = response.headers['Authorization'].split(' ').last
-      hmac_secret = ENV['DEVISE_JWT_SECRET_KEY']
-      decoded_token = JWT.decode token, hmac_secret, true
+      decoded_token = decoded_jwt_token_from_response(response)
       expect(decoded_token.first['sub']).to be_present
+    end
+
+    it 'token lifetime is 24h' do
+      decoded_token = decoded_jwt_token_from_response(response)
+      expiration_time = Time.zone.at(decoded_token.first['exp'])
+      expect(Time.current + 23.hours < expiration_time).to be_truthy
+      expect(Time.current + 24.hours < expiration_time).to be_falsy
     end
   end
 
   context 'when login params are incorrect' do
     before { post url }
     it_behaves_like 'unauthorized'
+  end
+
+  def decoded_jwt_token_from_response(response)
+    token = response.headers['Authorization'].split(' ').last
+    hmac_secret = ENV['DEVISE_JWT_SECRET_KEY']
+    JWT.decode token, hmac_secret, true
   end
 end

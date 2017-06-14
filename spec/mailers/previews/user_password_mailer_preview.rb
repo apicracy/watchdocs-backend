57a8c6510045402e5bfda66510b3ef1289d31526
mazikwyry <a.mazur@exlabs.co.uk>
2017-06-14 08:09:09 +0200

    trigger tets

--- spec/mailers/previews/user_password_mailer_preview.rb
+++ spec/mailers/previews/user_password_mailer_preview.rb
@@ -1,7 +1,7 @@
 class UserPasswordMailerPreview < ActionMailer::Preview
   def reset_instructions
     user = User.first
-    token = '8c7w4ba98n73wxr8oa3nx3ha'
+    token = '8c7w4ba98nd73wxr8oa3nx3ha'
     UserPasswordMailer.reset_instructions(user.id, token)
   end
 end

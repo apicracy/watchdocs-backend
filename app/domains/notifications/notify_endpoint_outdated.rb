341c97c9d017559333b741869dc7aff1e75258da
Krzysztof Szromek <k.szromek@exlabs.pl>
2017-07-07 14:20:31 +0200

    Fix message

--- app/domains/notifications/notify_endpoint_outdated.rb
+++ app/domains/notifications/notify_endpoint_outdated.rb
@@ -1,18 +1,33 @@
 module Notifications
   class NotifyEndpointOutdated
-    attr_reader :channels, :endpoint_id
+    attr_reader :channels, :endpoint, :project
 
     def initialize(project_id, endpoint_id)
       users        = Projects::FetchUsers.new.call(project_id)
       @channels    = Channel.where(user_id: users.select(:id))
-      @endpoint_id = endpoint_id
+      @endpoint = ::Endpoint.find(endpoint_id)
+      @project = ::Project.find(project_id)
     end
 
     def call
-      message = "Endpoint id: #{endpoint_id} has been broken."
+      message = "Endpoint #{endpoint_name} is outdated"
       SendPushNotification.new(channels: channels.push_notification, message: message).call
-      SendSlackNotification.new(channels: channels.slack, message: message).call
+      SendSlackNotification.new(channels: channels.slack, message: slack_message).call
       # Other providers...
+    end
+
+    private
+
+    def slack_message
+      "*#{project.name}*: #{endpoint_name} is outdated. Go to #{endpoint_path} to review changes."
+    end
+
+    def endpoint_name
+      "[#{endpoint.http_method}] #{endpoint.url}"
+    end
+
+    def endpoint_path
+      "#{ENV['FRONTEND_DOMAIN']}/#{project.slug}/editor/endpoint/#{endpoint.id}"
     end
   end
 end

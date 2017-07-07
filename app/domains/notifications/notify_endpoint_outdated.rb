6df86fed5e800337ebb6beaec4efced1aa6de087
Krzysztof Szromek <k.szromek@exlabs.pl>
2017-07-07 14:00:08 +0200

    Fix most urgent errors

--- app/domains/notifications/notify_endpoint_outdated.rb
+++ app/domains/notifications/notify_endpoint_outdated.rb
@@ -10,7 +10,7 @@ module Notifications
 
     def call
       message = "Endpoint id: #{endpoint_id} has been broken."
-      SendPushNotification.new(channels: channels.push_notifications, message: message).call
+      SendPushNotification.new(channels: channels.push_notification, message: message).call
       SendSlackNotification.new(channels: channels.slack, message: message).call
       # Other providers...
     end
